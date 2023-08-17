9a07c3fb26deb5c95175fc9e57e4d5ccdad04e8f
john-nash-rs <rohitsingh20121992@gmail.com>
2023-08-17 16:16:08 +0530

    first commit

---
+++ src/UserDashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserProfile from './UserProfile';
+
+function UserDashboard() {
+
+  return (
+    <div>
+      <h1>User Dashboard</h1>
+      <UserProfile />
+    </div>
+  );
+}
+
+export default UserDashboard;
